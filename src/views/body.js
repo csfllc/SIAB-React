436396e54dfbb4516eed1898d8d07d5e05c9a1cf
csfllc <githubcf@ckh.email>
2024-09-03 13:23:17 +0100

    Commit from TeleportHQ

---
+++ src/views/body.js
@@ -0,0 +1,494 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './body.css'
+
+const Body = (props) => {
+  return (
+    <div className="body-container10">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="body-body1">
+        <div className="body-group1">
+          <div className="body-body-row-data">
+            <span className="body-text100">
+              <span>startup in a box</span>
+            </span>
+            <span className="body-text102">
+              <span>your</span>
+            </span>
+          </div>
+        </div>
+        <div className="body-container11">
+          <div className="body-table">
+            <div className="body-body2">
+              <div className="body-row1">
+                <div className="body-data10">
+                  <span className="body-text104 www.dolekemp96.orgInterBold15.33">
+                    <span>
+                      Featured
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: ' ',
+                        }}
+                      />
+                    </span>
+                  </span>
+                  <span className="body-text106 www.dolekemp96.orgInterBold15.05">
+                    <span>Products</span>
+                  </span>
+                  <div className="body-container12">
+                    <div className="body-container13">
+                      <div className="body-container14">
+                        <span className="body-text108">
+                          <span>INDIE RELEASES</span>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="body-container15">
+                    <div className="body-mispelled1">
+                      <span className="body-text110 www.dolekemp96.orgInterRegular8.28underline">
+                        <span>Latest News and</span>
+                      </span>
+                      <span className="body-text112 www.dolekemp96.orgInterRegular8.13underline">
+                        <span>Updates</span>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="body-container16">
+                    <div className="body-container17">
+                      <div className="body-container18">
+                        <span className="body-text114">
+                          <span>MAINSTREAM</span>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="body-container19">
+                    <span className="body-text116 www.dolekemp96.orgInterRegular8.91underline">
+                      <span>K-12 Education</span>
+                    </span>
+                  </div>
+                  <div className="body-container20">
+                    <span className="body-text118 www.dolekemp96.orgInterRegular8.44underline">
+                      <span>Technology and</span>
+                    </span>
+                  </div>
+                  <div className="body-container21">
+                    <span className="body-text120 www.dolekemp96.orgInterRegular8.28underline">
+                      <span>Internet</span>
+                    </span>
+                  </div>
+                  <div className="body-container22">
+                    <div className="body-container23">
+                      <div className="body-container24">
+                        <span className="body-text122">
+                          <span>OLD SCHOOL</span>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="body-datadividergif1"></div>
+                <div className="body-data11">
+                  <img
+                    alt="image19161"
+                    src="/external/image19161-41sm-200h.png"
+                    className="body-image1"
+                  />
+                  <div className="body-container25">
+                    <span className="body-text124">
+                      <span>sign up for chatgpt</span>
+                    </span>
+                  </div>
+                  <div className="body-container26">
+                    <span className="body-text126 www.dolekemp96.orgInterRegular8.59">
+                      <span>sign up for claude</span>
+                    </span>
+                  </div>
+                  <span className="body-text128">
+                    <span>sign up for gemini</span>
+                  </span>
+                </div>
+                <div className="body-data12">
+                  <img
+                    alt="image343"
+                    src="/external/image343-5as-200h.png"
+                    className="body-image"
+                  />
+                  <div className="body-container27">
+                    <span className="body-text130">
+                      <span>reserve a domain</span>
+                    </span>
+                  </div>
+                </div>
+                <div className="body-data13">
+                  <img
+                    alt="image34216"
+                    src="/external/image34216-m5nu-200h.png"
+                    className="body-image3"
+                  />
+                  <div className="body-container28">
+                    <span className="body-text132 www.dolekemp96.orgInterRegular8.59">
+                      <span>register a</span>
+                      <br></br>
+                      <span>
+                        wyoming llc
+                        <span
+                          dangerouslySetInnerHTML={{
+                            __html: ' ',
+                          }}
+                        />
+                      </span>
+                    </span>
+                  </div>
+                </div>
+                <div className="body-datadividergif2"></div>
+                <div className="body-data14">
+                  <span className="body-text136 www.dolekemp96.orgInterBold15.75">
+                    <span>Featured</span>
+                  </span>
+                  <span className="body-text138 www.dolekemp96.orgInterBold15.47">
+                    <span>Newsletters</span>
+                  </span>
+                  <div className="body-container29">
+                    <span className="body-text140 www.dolekemp96.orgInterRegular11.17">
+                      <span>August 18, 2024</span>
+                      <br></br>
+                      <span></span>
+                    </span>
+                  </div>
+                  <div className="body-container30">
+                    <div className="body-container31">
+                      <span className="body-text144">
+                        <span>TODAY’S READS</span>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="body-container32">
+                    <div className="body-mispelled2">
+                      <span className="body-text146 www.dolekemp96.orgInterRegular8.75underline">
+                        <span>Follow along on the</span>
+                      </span>
+                      <span className="body-text148 www.dolekemp96.orgInterRegular8.59underline">
+                        <span>Victory Marathon</span>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="body-container33">
+                    <span className="body-text150 www.dolekemp96.orgInterRegular8.59underline">
+                      <span>Calculate your tax cut!</span>
+                    </span>
+                  </div>
+                  <div className="body-container34"></div>
+                  <div className="body-container35">
+                    <div className="body-container36">
+                      <div className="body-container37">
+                        <span className="body-text152">
+                          <span>TRENDING ON X</span>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="body-container38">
+                    <span className="body-text154 www.dolekemp96.orgInterRegular7.97underline">
+                      <span>Speeches</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="body-row2">
+                <div className="body-data15">
+                  <img
+                    alt="image24115"
+                    src="/external/image24115-hw9f-200h.png"
+                    className="body-image2"
+                  />
+                  <div className="body-container39">
+                    <span className="body-text156 www.dolekemp96.orgInterRegular8.59">
+                      <span>
+                        open a business
+                        <span
+                          dangerouslySetInnerHTML={{
+                            __html: ' ',
+                          }}
+                        />
+                      </span>
+                      <br></br>
+                      <span>checking account</span>
+                    </span>
+                  </div>
+                </div>
+                <div className="body-data16">
+                  <img
+                    alt="image44316"
+                    src="/external/image44316-g1s-200h.png"
+                    className="body-image4"
+                  />
+                  <div className="body-container40">
+                    <span className="body-text160 www.dolekemp96.orgInterRegular8.59">
+                      <span>
+                        create a business
+                        <span
+                          dangerouslySetInnerHTML={{
+                            __html: ' ',
+                          }}
+                        />
+                      </span>
+                      <br></br>
+                      <span>email account</span>
+                    </span>
+                  </div>
+                </div>
+                <div className="body-data17">
+                  <img
+                    alt="image54416"
+                    src="/external/image54416-dukh-200h.png"
+                    className="body-image5"
+                  />
+                  <div className="body-container41">
+                    <span className="body-text164 www.dolekemp96.orgInterRegular8.59">
+                      <span>create your website</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="body-row3">
+                <div className="body-data18">
+                  <div className="body-container42">
+                    <span className="body-text166 www.dolekemp96.orgInterRegular8.59">
+                      <span>
+                        create a business
+                        <span
+                          dangerouslySetInnerHTML={{
+                            __html: ' ',
+                          }}
+                        />
+                      </span>
+                      <br></br>
+                      <span>phone number</span>
+                    </span>
+                  </div>
+                  <img
+                    alt="image64517"
+                    src="/external/image64517-8ohj-200h.png"
+                    className="body-image6"
+                  />
+                </div>
+                <div className="body-data19">
+                  <img
+                    alt="image74517"
+                    src="/external/image74517-fep7-200h.png"
+                    className="body-image7"
+                  />
+                  <div className="body-container43">
+                    <span className="body-text170">
+                      <span>sign up for x</span>
+                    </span>
+                  </div>
+                  <div className="body-container44">
+                    <span className="body-text172 www.dolekemp96.orgInterRegular8.59">
+                      <span>sign up for tiktok</span>
+                    </span>
+                  </div>
+                  <span className="body-text174">
+                    <span>sign up for insta</span>
+                  </span>
+                </div>
+                <div className="body-data20">
+                  <div className="body-container45">
+                    <span className="body-text176 www.dolekemp96.orgInterRegular8.59">
+                      <span>
+                        sign up for
+                        <span
+                          dangerouslySetInnerHTML={{
+                            __html: ' ',
+                          }}
+                        />
+                      </span>
+                      <br></br>
+                      <span>morning brew</span>
+                    </span>
+                  </div>
+                  <img
+                    alt="image84618"
+                    src="/external/image84618-g3x8-200h.png"
+                    className="body-image8"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="body-container46">
+          <span className="body-text180">
+            <span>
+              $ ! @ ♭ $ ! @ ♭ $ ! @ ♭
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </span>
+          <span className="body-text182 www.dolekemp96.orgInterRegular8.91underline"></span>
+        </div>
+        <div className="body-container47">
+          <span className="body-text183 www.dolekemp96.orgInterRegular15.33">
+            <span>
+              everything you need to start your business on a single webpage.
+            </span>
+          </span>
+        </div>
+        <div className="body-container48">
+          <span className="body-text185">
+            <span className="body-text186">About the Team</span>
+            <span className="body-text187">
+              {' '}
+              |
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="body-text188">Dole Interactive</span>
+            <span className="body-text189">
+              {' '}
+              |
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="body-text190">Newsroom</span>
+            <span className="body-text191">
+              {' '}
+              |
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="body-text192">
+              The Dole Agenda
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="body-text193">
+              |
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="body-text194">On the Campaign Trail</span>
+            <span className="body-text195">
+              {' '}
+              |
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span>Get Involved</span>
+          </span>
+        </div>
+        <div className="body-container49"></div>
+        <div className="body-container50">
+          <span className="body-text197 www.dolekemp96.orgInterRegular8.75">
+            <span>
+              Originally Paid for by Dole Kemp &apos;96 Campaign Committee
+            </span>
+          </span>
+        </div>
+        <div className="body-container51">
+          <span className="body-text199">
+            <span className="body-text200">
+              Original Web Design by
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span>Presage Internet Campaigns</span>
+          </span>
+        </div>
+        <div className="body-container52">
+          <span className="body-text202">
+            <span className="body-text203">
+              To Learn more about Bob Dole, Please Visit the
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span>Dole Institute</span>
+          </span>
+        </div>
+        <div className="body-container53">
+          <span className="body-text205">
+            <span className="body-text206">
+              This Web Site is Presented for Educational Purposes by
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span>4President.org</span>
+          </span>
+        </div>
+      </div>
+      <div className="body-container54">
+        <span className="body-text208">
+          <span className="body-text209">Homepage</span>
+          <span className="body-text210">
+            {' '}
+            |
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span className="body-text211">Register</span>
+          <span className="body-text212">
+            {' '}
+            |
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span className="body-text213">Volunteers</span>
+          <span className="body-text214">
+            {' '}
+            |
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span>Donations</span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Body
